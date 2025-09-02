10c75692fff8f3a3f22b9571c9a51f2738cec499
Aditya-9570 <aditya2prakash@gmail.com>
2025-09-02 14:27:13 +0530

    All done

---
+++ vite-project/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=' text-orange-400  text-center font-bold m-4 p-5 text-5xl '>
+      ToDo List
+    </div>
+  )
+}
+
+export default Header
